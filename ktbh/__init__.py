5421ca3c6a059a6466d29d726c827bc4c96653d8
Martin Keegan <martin@no.ucant.org>
2013-02-25 19:50:06 +0000

    config file; add lp handler

--- ktbh/__init__.py
+++ ktbh/__init__.py
@@ -1,11 +1,10 @@
 import pika
 import json
+import landing_page
 
-OUT_QUEUE = "ktbh_landing_pages"
-
-def hand_off(out_queue, body):
+def hand_off(amqp_host, out_queue, body):
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost'))
+        pika.ConnectionParameters(host=amqp_host))
     channel = connection.channel()
     channel.queue_declare(queue=out_queue, durable=True)
     channel.basic_publish(exchange='',
@@ -14,9 +13,63 @@ def hand_off(out_queue, body):
                           properties=pika.BasicProperties(delivery_mode=2))
     connection.close()
 
-def add_landing_page(url):
+def add_landing_page(url, config):
+    amqp_host = config.get("main", "amqp_host")
+    out_queue = config.get("main", "lp_queue")
+
     payload = json.dumps({
             "url": url
             })
-    hand_off(OUT_QUEUE, payload)
+    hand_off(amqp_host, out_queue, payload)
+
+def get_connection(host):
+    count = 0.4
+    while count < 60:
+        try:            
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=host))
+            return connection
+        except:
+            time.sleep(count)
+            count *= 1.7
+    sys.exit(1)
+
+def handle_queue(amqp_host, queue_name, callback_fn):
+    try:
+        connection = get_connection(amqp_host)
+        channel = connection.channel()
+        channel.queue_declare(queue=queue_name, durable=True)
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(callback_fn, queue=queue_name)
+        channel.start_consuming()
+    except:
+        pass
+    finally:
+        connection.close()
+
+def examine_landing_pages(config):
+    out_queue = config.get("main", "lp_queue")
+    url_queue = config.get("main", "url_queue")
+    broken_queue = config.get("main", "broken_lp_queue")
+    amqp_host = config.get("main", "amqp_host")
+
+    def callback(ch, method, properties, body):
+        try:
+            args = json.loads(body)
+            url = args["url"]
+            count = 0
+            for text, href in landing_page.scrape(url):
+                payload = json.dumps({
+                        "link_text": text,
+                        "link_href": href
+                        })
+                hand_off(amqp_host, url_queue, payload)
+                count += 1
+            if count == 0:
+                hand_off(amqp_host, broken_queue, json.dumps({"url": url}))
+        finally:
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+
+    while True:
+        handle_queue(amqp_host, out_queue, callback)
     
---
+++ ktbh/landing_page.py
@@ -0,0 +1,49 @@
+
+import sys
+import lxml.html           
+import requests
+import socket
+
+def link_data(root):
+    for a in root.cssselect("a"):
+        text = a.text
+        href = a.get("href")
+
+        if href is None:
+            continue
+        yield (text, href)
+
+def csv_links(root):
+    for text, href in link_data(root):
+        if "csv" in href:
+            yield (text, href)
+
+def csv_text_links(root):
+    for text, href in link_data(root):
+        if text is None:
+            continue
+        if "csv" in text.lower():
+            yield (text, href)
+
+def scrape(url):
+    url = url.strip()
+    try:
+        html = requests.get(url, timeout=5.0).text
+    except socket.timeout:
+        return None
+    except requests.exceptions.Timeout:
+        return None
+    try:
+        root = lxml.html.fromstring(html)
+    except ValueError:
+        return None
+
+    links = [ l for l in csv_links(root) ]
+    if len(links) > 0:
+        return csv_links(root)
+
+    links = [ l for l in csv_text_links(root) ]
+    if len(links) > 0:
+        return csv_text_links(root)
+
+    raise StopIteration
