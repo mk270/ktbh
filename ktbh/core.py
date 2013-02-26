ed20178f0429318d66c03b066e5f5fc988a68f2a
Martin Keegan <martin@no.ucant.org>
2013-02-26 20:47:11 +0000

    move waterfall out of ktbh core

--- ktbh/core.py
+++ ktbh/core.py
@@ -1,85 +1,12 @@
 import sys
-import time
 import psycopg2
-import pika
 import json
 import landing_page
-
-class PipeRouter(object):
-    def __init__(self, amqp_host):
-        self.amqp_host = amqp_host
-
-    def hand_off_json(self, queue, args):
-        return self.hand_off(queue, json.dumps(args))
-
-    def hand_off(self, queue, body):
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.amqp_host))
-        channel = connection.channel()
-        channel.queue_declare(queue=queue, durable=True)
-        channel.basic_publish(exchange='',
-                              routing_key=queue,
-                              body=body,
-                              properties=pika.BasicProperties(delivery_mode=2))
-        connection.close()
-
-    def get_connection(self, host):
-        count = 0.4
-        while count < 60:
-            try:            
-                connection = pika.BlockingConnection(
-                    pika.ConnectionParameters(host=host))
-                return connection
-            except:
-                time.sleep(count)
-                count *= 1.7
-        sys.exit(1)
-
-    def handle_queue(self, queue_name, callback_fn):
-        connection = self.get_connection(self.amqp_host)
-        try:
-            channel = connection.channel()
-            channel.queue_declare(queue=queue_name, durable=True)
-            channel.basic_qos(prefetch_count=1)
-            channel.basic_consume(callback_fn, queue=queue_name)
-            channel.start_consuming()
-        except:
-            pass
-        finally:
-            connection.close()
-
-    def handle_queue_forever(self, queue_name, callback_fn):
-        while True:
-            self.handle_queue(queue_name, callback_fn)
-
-    def create_handler(self, f, errors_queue):
-        def callback(ch, method, properties, body):
-            try:
-                results = f(body)
-            except:
-                error_msg = {
-                    "error": {
-                        "type": sys.exc_info()[0],
-                        "err_str": sys.exc_info()[1],
-                        "orig_body": body
-                        }
-                    }
-                results = [ (errors_queue, error_msg) ]
-            try:
-                for r in results:
-                    queue, msg = r
-                    self.hand_off_json(queue, msg)
-            finally:
-                ch.basic_ack(delivery_tag = method.delivery_tag)
-        return callback
-
-    def route(self, callback=None, input_queue=None, error_queue=None):
-        cb = self.create_handler(callback, error_queue)
-        self.handle_queue_forever(input_queue, cb)
+import waterfall
 
 class KTBH(object):
     def __init__(self, config):
-        self.router = PipeRouter(config.get("main", "amqp_host"))
+        self.router = waterfall.PipeRouter(config.get("main", "amqp_host"))
         self.out_queue = config.get("main", "lp_queue")
         self.broken_queue = config.get("main", "broken_lp_queue")
         self.url_queue = config.get("main", "url_queue")
