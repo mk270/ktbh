347a30fa80696d5c24004914c59df483cc9f0bd0
Martin Keegan <martin@no.ucant.org>
2013-03-23 21:18:10 +0000

    get rid of queue config

--- ktbh/core.py
+++ ktbh/core.py
@@ -4,22 +4,13 @@ class KTBH(object):
     def __init__(self, config):
         self.router = waterfall.PipeRouter(config.get("main", "amqp_host"))
 
-        self.queues = {}
-        for q_name, conf_section, conf_item in [
-            ("out", "main", "lp_queue"),
-            ("broken", "main", "broken_lp_queue"),
-            ("url", "main", "url_queue"),
-            ("schema", "main", "schema_queue"),
-            ("download", "main", "download_queue")
-            ]:
-            self.queues[q_name] = config.get(conf_section, conf_item)
-        self.queues["error"] = "ktbh_errors"
+        self.queue_prefix = config.get("main", "queue_prefix")
 
     def get_queue(self, queue_name):
-        return self.queues[queue_name]
+        return self.queue_prefix + queue_name
 
     def list_queues(self):
-        return self.queues.values()
+        assert False
 
     def delete_all_queues(self):
         for q in self.list_queues():
@@ -39,4 +30,4 @@ class KTBH(object):
     def run_pipe(self, input_queue, callback):
         self.router.route(callback=self.wrap_callback(callback),
                           input_queue=self.get_queue(input_queue),
-                          error_queue=self.get_queue("error"))
+                          error_queue=self.get_queue("errors"))
